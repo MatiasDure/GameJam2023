2f0509a426163be4eb318dbec4f8372f7628969f
Matias Dure <497232@student.saxion.nl>
2023-02-03 23:08:31 +0100

    Spawn + movement

---
+++ Assets/SpawnSprout.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Threading;
+using UnityEngine;
+
+public class SpawnSprout : MonoBehaviour
+{
+    [SerializeField] float spawnDelay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator Spawn()
+    {
+        while(true)
+        {
+            var sprout = SproutPool.Instance.GetPooledRoot();
+            sprout.gameObject.SetActive(true);
+            sprout.transform.position = new Vector3(0, 0, 0);
+
+            yield return new WaitForSeconds(spawnDelay);
+        }
+    }
+}
